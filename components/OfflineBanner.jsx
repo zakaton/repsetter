e4b8584c9bdbaffd9736daf1079ab5e905642aff
Zack <zack@ukaton.com>
2022-06-07 20:28:20 -0700

    fixes

--- components/OfflineBanner.jsx
+++ components/OfflineBanner.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { XCircleIcon } from '@heroicons/react/solid';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import MyLink from '../components/MyLink';
