a8097e4346c55c038ab3ed802931968c17a05d30
Zack <zack@ukaton.com>
2022-06-11 20:19:39 -0700

    Update setup-account.js

--- pages/api/account/setup-account.js
+++ pages/api/account/setup-account.js
@@ -24,7 +24,6 @@ export default async function handler(req, res) {
     .update({
       stripe_customer: customer.id,
       stripe_account: account.id,
-      notifications: ["email_subscription_receipt"],
     })
     .eq("id", req.body.record.id);
 
