6dcd76547e5ce03980b075dca89af83a5e63ff15
Zack <zack@ukaton.com>
2022-06-11 19:31:51 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   reactStrictMode: false,
   pwa: {
-    dest: 'public',
+    dest: "public",
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === "development",
   },
   i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
+    locales: ["en"],
+    defaultLocale: "en",
   },
   compiler:
-    process.env.NODE_ENV === 'production'
+    false && process.env.NODE_ENV === "production"
       ? {
           removeConsole: {
-            exclude: ['error'],
+            exclude: ["error"],
           },
         }
       : {},
