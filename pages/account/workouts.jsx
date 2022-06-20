52107704c212462003c3ddd12f467cd8a1301ce0
Zack <zack@ukaton.com>
2022-06-20 13:00:39 -0700

    Update workouts.jsx

--- pages/account/workouts.jsx
+++ pages/account/workouts.jsx
@@ -1,8 +1,8 @@
 import { getAccountCalendarLayout } from "../../components/layouts/AccountCalendarLayout";
-import { useUser } from "../../context/user-context";
 import Head from "next/head";
-
+import { useUser } from "../../context/user-context";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import { useState, useEffect } from "react";
 
 const months = [
   "January",
@@ -19,52 +19,8 @@ const months = [
   "December",
 ];
 
-const years = [2022, 2023];
-
-const days = [
-  { date: "2021-12-27" },
-  { date: "2021-12-28" },
-  { date: "2021-12-29" },
-  { date: "2021-12-30" },
-  { date: "2021-12-31" },
-  { date: "2022-01-01", isCurrentMonth: true },
-  { date: "2022-01-02", isCurrentMonth: true },
-  { date: "2022-01-03", isCurrentMonth: true },
-  { date: "2022-01-04", isCurrentMonth: true },
-  { date: "2022-01-05", isCurrentMonth: true },
-  { date: "2022-01-06", isCurrentMonth: true },
-  { date: "2022-01-07", isCurrentMonth: true },
-  { date: "2022-01-08", isCurrentMonth: true },
-  { date: "2022-01-09", isCurrentMonth: true },
-  { date: "2022-01-10", isCurrentMonth: true },
-  { date: "2022-01-11", isCurrentMonth: true },
-  { date: "2022-01-12", isCurrentMonth: true, isToday: true },
-  { date: "2022-01-13", isCurrentMonth: true },
-  { date: "2022-01-14", isCurrentMonth: true },
-  { date: "2022-01-15", isCurrentMonth: true },
-  { date: "2022-01-16", isCurrentMonth: true },
-  { date: "2022-01-17", isCurrentMonth: true },
-  { date: "2022-01-18", isCurrentMonth: true },
-  { date: "2022-01-19", isCurrentMonth: true },
-  { date: "2022-01-20", isCurrentMonth: true },
-  { date: "2022-01-21", isCurrentMonth: true },
-  { date: "2022-01-22", isCurrentMonth: true, isSelected: true },
-  { date: "2022-01-23", isCurrentMonth: true },
-  { date: "2022-01-24", isCurrentMonth: true },
-  { date: "2022-01-25", isCurrentMonth: true },
-  { date: "2022-01-26", isCurrentMonth: true },
-  { date: "2022-01-27", isCurrentMonth: true },
-  { date: "2022-01-28", isCurrentMonth: true },
-  { date: "2022-01-29", isCurrentMonth: true },
-  { date: "2022-01-30", isCurrentMonth: true },
-  { date: "2022-01-31", isCurrentMonth: true },
-  { date: "2022-02-01" },
-  { date: "2022-02-02" },
-  { date: "2022-02-03" },
-  { date: "2022-02-04" },
-  { date: "2022-02-05" },
-  { date: "2022-02-06" },
-];
+const getNumberOfDaysInMonth = (date) =>
+  new Date(date.getUTCFullYear(), date.getUTCMonth() + 1, 0).getUTCDate();
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -72,6 +28,87 @@ function classNames(...classes) {
 
 export default function Workouts() {
   const { user } = useUser();
+  const [clientEmail, setClientEmail] = useState();
+
+  const yearsRange = [2022, 2023];
+
+  const years = [];
+  // FIX - set range based on workouts range
+  for (let year = yearsRange[0]; year <= yearsRange[1]; year++) {
+    years.push(year);
+  }
+
+  const currentDate = new Date();
+
+  const [selectedDate, setSelectedDate] = useState();
+  const [previouslySelectedDate, setPreviouslySelectedDate] = useState();
+  useEffect(() => {
+    // FILL - get day/month/year from query params
+    setSelectedDate(currentDate);
+    setClientEmail(user.email);
+  }, []);
+
+  const [calendar, setCalendar] = useState();
+  const populateCalendar = () => {
+    console.log("populating calendar");
+
+    const newCalendar = [];
+    newCalendar.push(selectedDate);
+
+    const dateBeforeSelectedDate = new Date(selectedDate);
+    const addPreviousDate = () => {
+      dateBeforeSelectedDate.setUTCDate(
+        dateBeforeSelectedDate.getUTCDate() - 1
+      );
+      newCalendar.unshift(new Date(dateBeforeSelectedDate));
+    };
+    while (
+      dateBeforeSelectedDate.getUTCDate() > 1 &&
+      dateBeforeSelectedDate.getUTCDate() <= selectedDate.getUTCDate()
+    ) {
+      addPreviousDate();
+    }
+    while (dateBeforeSelectedDate.getUTCDay() !== 0) {
+      addPreviousDate();
+    }
+
+    const numberOfDaysInMonth = getNumberOfDaysInMonth(selectedDate);
+    const dateAfterSelectedDate = new Date(selectedDate);
+    const addNextDate = () => {
+      dateAfterSelectedDate.setUTCDate(dateAfterSelectedDate.getUTCDate() + 1);
+      newCalendar.push(new Date(dateAfterSelectedDate));
+    };
+    while (
+      dateAfterSelectedDate.getUTCDate() >= selectedDate.getUTCDate() &&
+      dateAfterSelectedDate.getUTCDate() < numberOfDaysInMonth
+    ) {
+      addNextDate();
+    }
+    while (dateAfterSelectedDate.getUTCDay() !== 6) {
+      addNextDate();
+    }
+
+    setCalendar(newCalendar);
+  };
+
+  useEffect(() => {
+    if (
+      selectedDate &&
+      previouslySelectedDate &&
+      selectedDate.getUTCFullYear() ==
+        previouslySelectedDate.getUTCFullYear() &&
+      selectedDate.getUTCMonth() == previouslySelectedDate.getUTCMonth() &&
+      selectedDate.getUTCDate() == previouslySelectedDate.getUTCDate()
+    ) {
+      return;
+    }
+
+    if (selectedDate) {
+      populateCalendar();
+      setPreviouslySelectedDate(selectedDate);
+    }
+  }, [selectedDate, calendar]);
+
   return (
     <>
       <Head>
@@ -87,15 +124,17 @@ export default function Workouts() {
               <select
                 id="clientEmail"
                 className="mt-1 w-full rounded-md border-gray-300 py-1 pl-2 pr-10 text-base focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                defaultValue="Me"
+                value={clientEmail}
+                onInput={(e) => setClientEmail(e.target.value)}
               >
-                <option>Me</option>
-                <option>United States</option>
-                <option>Mexico</option>
+                <option value={user.email}>Me</option>
+                {/* FILL - set clients */}
               </select>
             </div>
             <p className="mt-2 text-sm text-gray-500">
-              View and Edit workouts for [YOU or CLIENT_EMAIL]
+              View and Edit{" "}
+              {clientEmail === user.email ? "your" : `${clientEmail}'s`}{" "}
+              Workouts
             </p>
           </div>
           <div className="mt-8 text-center lg:col-start-8 lg:col-end-13 lg:row-span-2 lg:row-start-1 lg:mt-0 xl:col-start-9">
@@ -103,6 +142,13 @@ export default function Workouts() {
               <button
                 type="button"
                 className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                onClick={() => {
+                  const newSelectedDate = new Date(selectedDate);
+                  newSelectedDate.setUTCMonth(
+                    newSelectedDate.getUTCMonth() - 1
+                  );
+                  setSelectedDate(newSelectedDate);
+                }}
               >
                 <span className="sr-only">Previous month</span>
                 <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -111,17 +157,29 @@ export default function Workouts() {
                 <select
                   id="month"
                   className="mt-1 rounded-md border-gray-300 py-1 pl-2 pr-8 font-semibold focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                  defaultValue="January"
+                  value={selectedDate?.getUTCMonth() || 0}
+                  onInput={(e) => {
+                    const newSelectedDate = new Date(selectedDate);
+                    newSelectedDate.setUTCMonth(e.target.selectedIndex);
+                    setSelectedDate(newSelectedDate);
+                  }}
                 >
-                  {months.map((month) => (
-                    <option key={month}>{month}</option>
+                  {months.map((month, index) => (
+                    <option key={month} value={index}>
+                      {month}
+                    </option>
                   ))}
                 </select>
 
                 <select
                   id="year"
                   className="mt-1 ml-4 rounded-md border-gray-300 py-1 pl-2 pr-8 font-semibold focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                  defaultValue="2022"
+                  value={selectedDate?.getUTCFullYear()}
+                  onInput={(e) => {
+                    const newSelectedDate = new Date(selectedDate);
+                    newSelectedDate.setUTCFullYear(e.target.value);
+                    setSelectedDate(newSelectedDate);
+                  }}
                 >
                   {years.map((year) => (
                     <option key={year}>{year}</option>
@@ -131,57 +189,83 @@ export default function Workouts() {
               <button
                 type="button"
                 className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                onClick={() => {
+                  const newSelectedDate = new Date(selectedDate);
+                  newSelectedDate.setUTCMonth(
+                    newSelectedDate.getUTCMonth() + 1
+                  );
+                  setSelectedDate(newSelectedDate);
+                }}
               >
                 <span className="sr-only">Next month</span>
                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
               </button>
             </div>
             <div className="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
+              <div>S</div>
               <div>M</div>
               <div>T</div>
               <div>W</div>
               <div>T</div>
               <div>F</div>
               <div>S</div>
-              <div>S</div>
             </div>
             <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
-              {days.map((day, dayIdx) => (
-                <button
-                  key={day.date}
-                  type="button"
-                  className={classNames(
-                    "py-1.5 hover:bg-gray-100 focus:z-10",
-                    day.isCurrentMonth ? "bg-white" : "bg-gray-50",
-                    (day.isSelected || day.isToday) && "font-semibold",
-                    day.isSelected && "text-white",
-                    !day.isSelected &&
-                      day.isCurrentMonth &&
-                      !day.isToday &&
-                      "text-gray-900",
-                    !day.isSelected &&
-                      !day.isCurrentMonth &&
-                      !day.isToday &&
-                      "text-gray-400",
-                    day.isToday && !day.isSelected && "text-blue-600",
-                    dayIdx === 0 && "rounded-tl-lg",
-                    dayIdx === 6 && "rounded-tr-lg",
-                    dayIdx === days.length - 7 && "rounded-bl-lg",
-                    dayIdx === days.length - 1 && "rounded-br-lg"
-                  )}
-                >
-                  <time
-                    dateTime={day.date}
-                    className={classNames(
-                      "mx-auto flex h-7 w-7 items-center justify-center rounded-full",
-                      day.isSelected && day.isToday && "bg-blue-600",
-                      day.isSelected && !day.isToday && "bg-gray-900"
-                    )}
-                  >
-                    {day.date.split("-").pop().replace(/^0/, "")}
-                  </time>
-                </button>
-              ))}
+              {calendar &&
+                calendar.map((date, dayIdx) => {
+                  const day = {
+                    date,
+                    isCurrentMonth:
+                      date.getUTCMonth() === selectedDate.getUTCMonth(),
+                    isSelected:
+                      date.getUTCFullYear() === selectedDate.getUTCFullYear() &&
+                      date.getUTCMonth() === selectedDate.getUTCMonth() &&
+                      date.getUTCDate() === selectedDate.getUTCDate(),
+                    isToday:
+                      date.getUTCFullYear() === currentDate.getUTCFullYear() &&
+                      date.getUTCMonth() === currentDate.getUTCMonth() &&
+                      date.getUTCDate() === currentDate.getUTCDate(),
+                  };
+                  return (
+                    <button
+                      key={day.date.toDateString()}
+                      type="button"
+                      onClick={() => {
+                        setSelectedDate(day.date);
+                      }}
+                      className={classNames(
+                        "py-1.5 hover:bg-gray-100 focus:z-10",
+                        day.isCurrentMonth ? "bg-white" : "bg-gray-50",
+                        (day.isSelected || day.isToday) && "font-semibold",
+                        day.isSelected && "text-white",
+                        !day.isSelected &&
+                          day.isCurrentMonth &&
+                          !day.isToday &&
+                          "text-gray-900",
+                        !day.isSelected &&
+                          !day.isCurrentMonth &&
+                          !day.isToday &&
+                          "text-gray-400",
+                        day.isToday && !day.isSelected && "text-blue-600",
+                        dayIdx === 0 && "rounded-tl-lg",
+                        dayIdx === 6 && "rounded-tr-lg",
+                        dayIdx === calendar.length - 7 && "rounded-bl-lg",
+                        dayIdx === calendar.length - 1 && "rounded-br-lg"
+                      )}
+                    >
+                      <time
+                        dateTime={day.date.toDateString()}
+                        className={classNames(
+                          "mx-auto flex h-7 w-7 items-center justify-center rounded-full",
+                          day.isSelected && day.isToday && "bg-blue-600",
+                          day.isSelected && !day.isToday && "bg-gray-900"
+                        )}
+                      >
+                        {day.date.getUTCDate()}
+                      </time>
+                    </button>
+                  );
+                })}
             </div>
             <button
               type="button"
