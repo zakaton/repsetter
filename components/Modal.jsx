c34ba00ba0b4b0f0f7ea511011947ad8bbd69391
Zack <zack@ukaton.com>
2022-07-02 06:17:59 -0700

    Update Modal.jsx

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -77,7 +77,7 @@ export default function Modal({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
-                <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
+                <div className="absolute top-0 right-0 block pt-4 pr-4">
                   <button
                     type="button"
                     className={classNames(
