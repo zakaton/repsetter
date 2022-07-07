1524b5bf8dc983163e7112cfeab8e9b12bcc922c
Zack <zack@ukaton.com>
2022-07-07 07:10:45 -0700

    sample donut chart

--- pages/account/progress.jsx
+++ pages/account/progress.jsx
@@ -7,6 +7,10 @@ import { useClient } from "../../context/client-context";
 import ExerciseTypesSelect from "../../components/account/modal/ExerciseTypesSelect";
 import Filters from "../../components/Filters";
 import { useSelectedExerciseType } from "../../context/selected-exercise-context";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const filterTypes = [];
 const orderTypes = [
@@ -79,6 +83,39 @@ export default function Progress() {
             setSelectedExerciseTypeName={setSelectedExerciseTypeName}
           />
         </Filters>
+
+        <div className="border-t border-gray-200"></div>
+
+        <div className="w-1/2">
+          <Doughnut
+            data={{
+              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+              datasets: [
+                {
+                  label: "# of Votes",
+                  data: [12, 19, 3, 5, 2, 3],
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                  ],
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
+        </div>
       </div>
     </>
   );
