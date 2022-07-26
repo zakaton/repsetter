5bb0347ad3f7f49a59c99fe20e9f4da30cb92878
Zack <zack@ukaton.com>
2022-07-26 11:58:09 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -140,21 +140,23 @@ export default function Header() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 pt-2 pb-4">
-              {navigation.map(({ name, href }) => (
-                <Disclosure.Button
-                  as="a"
-                  key={name}
-                  href={href}
-                  className={classNames(
-                    router.pathname === href
-                      ? "border-blue-500 bg-blue-50 text-blue-700"
-                      : "border-transparent  text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700",
-                    "block border-l-4 py-2 pl-3 pr-4 text-base font-medium"
-                  )}
-                >
-                  {name}
-                </Disclosure.Button>
-              ))}
+              {navigation
+                .filter(({ requiresUser }) => !requiresUser)
+                .map(({ name, href }) => (
+                  <Disclosure.Button
+                    as="a"
+                    key={name}
+                    href={href}
+                    className={classNames(
+                      router.pathname === href
+                        ? "border-blue-500 bg-blue-50 text-blue-700"
+                        : "border-transparent  text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700",
+                      "block border-l-4 py-2 pl-3 pr-4 text-base font-medium"
+                    )}
+                  >
+                    {name}
+                  </Disclosure.Button>
+                ))}
             </div>
           </Disclosure.Panel>
         </>
