6b62fb022bbf984944089233629cf886d9c1c95f
Zack <zack@ukaton.com>
2022-07-30 12:59:59 -0700

    text

--- pages/account/exercises.jsx
+++ pages/account/exercises.jsx
@@ -386,7 +386,7 @@ export default function Exercises() {
                 }`}
                 className="inline-flex items-center rounded-md border border-gray-300 bg-white px-2.5 py-1.5 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
               >
-                View Diary
+                Diary
               </MyLink>
             ),
           },
