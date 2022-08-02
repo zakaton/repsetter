d1fc4ece099e8596c19806d0bad1f5d0d0bc5de3
Zack <zack@ukaton.com>
2022-08-02 13:58:26 -0700

    Update Pagination.jsx

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -215,7 +215,7 @@ export default function Pagination({
         <div
           className={classNames(
             numberOfResults > numberOfResultsPerPage
-              ? "hidden sm:block"
+              ? "hidden xs:block"
               : "block"
           )}
         >
