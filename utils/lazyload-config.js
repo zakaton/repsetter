4b02d739bfe4371a83e056a5fadd65fa7ee347ab
Zack <zack@ukaton.com>
2022-09-19 05:36:29 -0700

    testing

--- utils/lazyload-config.js
+++ utils/lazyload-config.js
@@ -1,5 +1,11 @@
 const config = {
   elements_selector: ".lazy",
   threshold: 100,
+  callback_enter: (element) => {
+    console.log("enter", element);
+  },
+  callback_exit: (element) => {
+    console.log("Exit", element);
+  },
 };
 export default config;
