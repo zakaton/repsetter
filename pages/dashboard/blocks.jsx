048ab8838130bcb80d9e2a30585e4c2b42598959
Zack <zack@ukaton.com>
2022-09-28 10:23:09 -0700

    Update blocks.jsx

--- pages/dashboard/blocks.jsx
+++ pages/dashboard/blocks.jsx
@@ -76,6 +76,7 @@ export default function Blocks() {
         } blocks`}
         CreateResultModal={BlockModal}
         DeleteResultModal={DeleteBlockModal}
+        editTitle="Update"
         EditResultModal={BlockModal}
         resultMap={(result) => [
           false &&
@@ -108,7 +109,7 @@ export default function Blocks() {
                 href={`/dashboard/diary?block=${result.id}`}
                 className="inline-flex items-center rounded-md border border-gray-300 bg-white px-2.5 py-1.5 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
               >
-                View<span className="sr-only"> block</span>
+                Edit<span className="sr-only"> block</span>
               </MyLink>
             ),
           },
